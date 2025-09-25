543b74a361e2074a9099bbb1fa5bfeb2d13ee758
miiichelleo <michelle.otiji@fh-potsdam.de>
2025-09-25 22:29:54 +0200

    25.09

--- sketch.js
+++ sketch.js
@@ -9,11 +9,8 @@ let maxFrames = 60; // 10 seconds at 60 FPS
 
 let resolution =70; // Sphere detail (lat/lon divisions)
 function preload() {
-
-  document.addEventListener("click", () => {
-    const audio = new Audio("abc.mp3");
-    audio.play();
-});}
+  audio = loadSound('abc.mp3');
+}
 
 function setup() {
   const canvas = createCanvas(950, 950, WEBGL);
